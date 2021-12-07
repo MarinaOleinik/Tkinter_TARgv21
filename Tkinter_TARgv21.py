436845f2ec45a4d6e06a6cde9b80da3401babf3b
marina.oleinik <marina.oleinik@HP-CZC6297BB4>
2021-12-07 11:33:02 +0200

    Toplevel. Menu. Tabs

--- Tkinter_TARgv21.py
+++ Tkinter_TARgv21.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 klik=0
 def klikker(event):
     global klik
@@ -18,10 +19,48 @@ def text_to_lbl(event):
 def valik():
     val=str(var.get())+", " #value 1,2 või 3
     ent.insert(END,val)
+
+def uus_aken(ind:int):
+    uusaken=Toplevel()
+    tabs=ttk.Notebook(uusaken)
+    texts=["snake1.gif","1.gif","2.gif","3.gif","4.gif","5.gif","6.gif","7.gif","8.gif","9.gif"]
+    textn=["snake1.gif","1.gif","2.gif","3.gif","4.gif","5.gif","6.gif","7.gif","8.gif","9.gif"]
+    tab=[]
+    cann=[]
+    for i in range(len(texts)):
+        tab.append("tab"+str(i))
+        tab[i]=Frame(tabs)
+        tabs.add(tab[i],text=texts[i])#,image=img1)
+        textn[i]=PhotoImage(file=texts[i]).subsample(1)
+        cann.append("cann"+str(i))
+        cann[i]=Canvas(tab[i],height=200,width=300,bg="red")
+        cann[i].create_image(0,0,image=textn[i], anchor=NW)
+        cann[i].pack()
+
+    tabs.grid(row=0,column=0)
+    tabs.select(ind)
+    uusaken.mainloop()
+
+
+
 aken=Tk() #Akna loomine
 aken.title("Akna nimetus") #Akna pealkiri
 aken.geometry("600x400") #Akna suurus
+menu=Menu(aken)
+aken.config(menu=menu)
+m1=Menu(menu)
+menu.add_cascade(label="Tabs",menu=m1)
+m1.add_command(label="Tab1",accelerator="Command+A", command=lambda:uus_aken(0))
+m1.add_command(label="Tab2",command=lambda:uus_aken(1))
+m1.add_command(label="Tab3",command=lambda:uus_aken(2))
+m1.add_command(label="Tab4",command=lambda:uus_aken(3))
+m1.add_command(label="Tab5",command=lambda:uus_aken(4))
+m1.add_separator()
+
+
+
 btn=Button(aken,text="Vajuta siia",font="Arial 20",fg="green",bg="lightblue", width=20, height=3,relief=SUNKEN)#GROOVE, RAISED Loome nupp
+btn2=Button(aken,text="Veel aken",font="Arial 20",fg="green",bg="lightblue",command=lambda:uus_aken(0))
 lbl=Label(aken,text="...") #Pealkiri
 ent=Entry(aken,fg="blue",width=20,font="Arial 20",justify=CENTER) #Rida teksti sisestamiseks. tekxt on keskel
 var=IntVar() #StringVar()
@@ -39,4 +78,5 @@ ent.pack()
 r1.pack(side=LEFT)
 r2.pack(side=LEFT)
 r3.pack(side=LEFT)
+btn2.pack(side=RIGHT)
 aken.mainloop()
